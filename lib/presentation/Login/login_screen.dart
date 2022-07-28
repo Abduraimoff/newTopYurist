3f697f09d90034987d550b5beec8a704c33be0e1
Feruzbek <feruzbek6500@gmail.com>
2022-07-28 06:46:52 +0500

    UI splash screen done

--- lib/presentation/Login/login_screen.dart
+++ lib/presentation/Login/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_locales/flutter_locales.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:top_yurist/utils/colors.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -14,7 +15,7 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    ScreenUtil.init(context, designSize: const Size(360, 690));
+    ScreenUtil.init(context, designSize: const Size(375, 812));
     return Scaffold(
       body: SafeArea(
         child: Column(
@@ -34,32 +35,50 @@ class _LoginScreenState extends State<LoginScreen> {
             SizedBox(
               height: 37.h,
             ),
-            DropdownButton(
-              items: const [
-                DropdownMenuItem(
-                  value: "ru",
-                  child: Text('Русский'),
-                ),
-                DropdownMenuItem(
-                  value: "uz",
-                  child: Text('O\'zbek'),
-                ),
-                DropdownMenuItem(
-                  value: "uzc",
-                  child: Text('Узбек'),
-                ),
-                DropdownMenuItem(
-                  value: "en",
-                  child: Text('English'),
-                ),
-              ],
-              onChanged: (String? v) {
+            Container(
+              color: AppColors.scaffoldBackground,
+              child: DropdownButton(
+                style: Theme.of(context).textTheme.button,
+              underline: const SizedBox(),
+                items: const [
+                  DropdownMenuItem(
+                    value: "ru",
+                    child: Text('Русский'),
+                  ),
+                  DropdownMenuItem(
+                    value: "uz",
+                    child: Text('O\'zbek'),
+                  ),
+                  DropdownMenuItem(
+                    value: "uzc",
+                    child: Text('Узбек'),
+                  ),
+                  DropdownMenuItem(
+                    value: "en",
+                    child: Text('English'),
+                  ),
+                ],
+                onChanged: (String? v) {
 
-                Locales.change(context, v ?? "ru" );
-              },
-              onTap: () {},
-              value: Locales.lang,
-            )
+                  Locales.change(context, v ?? "ru" );
+                },
+                onTap: () {},
+                value: Locales.lang,
+              ),
+            ),
+            const Spacer(),
+            SizedBox(
+              height: 48.h,
+              width: 343.w,
+              child: ElevatedButton(
+                  onPressed: (){},  style: ElevatedButton.styleFrom(primary: const Color.fromRGBO(133, 141, 163, 0.1), elevation: 0, ),child:  LocaleText("i_lawyer", style: Theme.of(context).textTheme.headline3,),),
+            ),
+            SizedBox(height: 21.h,),
+            SizedBox(
+                height: 48.h,
+                width: 343.w,
+                child: ElevatedButton(onPressed: (){},style: ElevatedButton.styleFrom(primary: const Color.fromRGBO(133, 141, 163, 0.1), elevation: 0, ), child:  LocaleText("i_user", style: Theme.of(context).textTheme.headline3,))),
+            SizedBox(height: 30.h,)
           ],
         ),
       ),
--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 
 class AppColors{
   static const Color black = Color(0xFF18181C);
+  static const Color scaffoldBackground = Color(0xFFE4E4E8);
 }
