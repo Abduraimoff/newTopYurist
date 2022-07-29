6c1a9a0e860d9a32e5cdca1c83c5e807f9100123
Diyorjon825 <80413744+Diyorjon825@users.noreply.github.com>
2022-07-29 11:57:18 +0500

    bug fix

--- lib/bloc/cubit/profile_cubit_cubit.dart
+++ lib/bloc/cubit/profile_cubit_cubit.dart
@@ -2,8 +2,6 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:top_yurist/data/Models/user/user.dart';
 
-part 'profile_cubit_state.dart';
-
 class ProfileCubit extends Cubit<User> {
   ProfileCubit(initialState) : super(initialState);
 
